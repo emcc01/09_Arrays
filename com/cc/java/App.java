ae20363a69f7f15243838794696a6e6f4cabf638
Eva <e.moritz.comcave@gmail.com>
2023-05-26 10:53:57 +0200

    kommentiert

--- com/cc/java/App.java
+++ com/cc/java/App.java
@@ -6,87 +6,87 @@ public class App {
 
     public static void main(String[] args) {
 
-      	// // Deklaration: int
-		// int [] intArr = new int [4];
-		// output("Länge Array: " + intArr.length);	
+      	// Deklaration: int
+		int [] intArr = new int [4];  // 4 Elemente enthalten von Typ int, Arrays sind objekte
+		output("Länge Array: " + intArr.length);	
 		
-		// // Index
-		// output("3. Element " + intArr[2]);
+		// Index
+		output("3. Element " + intArr[2]); // ist erstmal 0 gesetzt weil noch nichts eingegeben wurde
 		
-		// intArr[0] = 10;
-		// output("1. Element " + intArr[0]);
-		// intArr[intArr.length - 1] = 11;
+		intArr[0] = 10;
+		output("1. Element " + intArr[0]);  // Elemente ändern und ausgeben
+		intArr[intArr.length - 1] = 11; // letztes Element ändern
 		
-		// output("------------------ ");
+		output("------------------ ");
 
-		// for (int i = 0; i < intArr.length; i++) {
-		// 	output("Element: " + i + " : "+ intArr[i]);
-		// }
+		for (int i = 0; i < intArr.length; i++) { // mit Zähler (Inkrement) ganzen Array ausgeben lassen
+			output("Element: " + i + " : "+ intArr[i]);
+		}
 			
 		// Deklaration: String
-		// String [] strArr = new String [2];
-		// String [] strArr = {"Hallo","Nikola"};
+		// String [] strArr = new String [2];  // Objektschreibweise, Inhalt muss danach noch eingegeben werden, s.o.
+		String [] strArr = {"Hallo","Nikola"};  // Literalschreibweise
 		
-		// for (int i = 0; i < strArr.length; i++) {
-		// 	output("Element: " + strArr[i]);
-		// }
+		for (int i = 0; i < strArr.length; i++) { // iterieren
+			output("Element: " + strArr[i]);
+		}
 		
 		
 		// Mehrdimensionale Arrays
 		// String [][] strMdArr = new String [2][2];
 		
-		// String [][] strMdArr = {{"Max","Mustermann"},{"Maxine","Musterfrau"}};
+		String [][] strMdArr = {{"Max","Mustermann"},{"Maxine","Musterfrau"}}; // Literalschreibweise
 		
 		// {{0,2,1},{1,0,0}}  x|y|z
 		
-		// output(strMdArr[0][0]);
-		// output(strMdArr[1][1]);
+		output(strMdArr[0][0]); // einzelne Elemente ausgeben lassen
+		output(strMdArr[1][1]);
 		
 		// // nested loops
-		// for (int i = 0; i < strMdArr.length; i++) {
-		// 	for (int j = 0; j < strMdArr.length; j++) {
-		// 		output("Indizes i/j: " + "i:" +  i + " j:"+ j + " " + strMdArr[i][j]);
-		// 	}
-		// }
+		for (int i = 0; i < strMdArr.length; i++) { // ganze Arrays ausgeben lassen
+			for (int j = 0; j < strMdArr.length; j++) {
+				output("Indizes i/j: " + "i:" +  i + " j:"+ j + " " + strMdArr[i][j]);
+			}
+		}
 		
-		// // Deklaration | Chars
-		// char [] letters = {'d','c','a','A','b'};
+		// Deklaration | Chars
+		char [] letters = {'d','c','a','A','b'}; //char ist primitive, wird in einzelnen Anführungszeichen
 	
-		// 	output("");
-		// 	output("---- vor Sortierung: ");
-		// 	for (int i = 0; i < letters.length; i++) {
-		// 		output(letters[i]);	
-		// 	}
+			output("");  //nur für Zeilenumbruch
+			output("---- vor Sortierung: "); //Ausgabe
+			for (int i = 0; i < letters.length; i++) {
+				output(letters[i]);	//überladene Methode, siehe ganz unten
+			}
 
 		// // Sortierfkt. --> sortiertes Array
-		// Arrays.sort(letters);  // Statische Klasse | Methode
+		Arrays.sort(letters);  // Statische Klasse | statische Methode wird aufgerufen
 
-		// output("");
-		// output("---- nach Sortierung: ");
-		// for (int i = 0; i < letters.length; i++) {
-		// 	output(letters[i]);	
-		// }
+		output("");
+		output("---- nach Sortierung: ");
+		for (int i = 0; i < letters.length; i++) { // Ausgabe sortiert
+			output(letters[i]);	
+		}
 
-		// 	output("");
-		// 	output("---- Suche: erfolgreich ----");
-		// 	// ... danach Suche (1)
-		// 	int searchIndex = Arrays.binarySearch(letters,'a');
-		// 	output("");
-		// 	output("---- Binäre Suche: ");
-		// 	output("searchIndex: " + searchIndex);
+			output("");
+			output("---- Suche: erfolgreich ----");
+			// ... danach Suche (1)
+			int searchIndex = Arrays.binarySearch(letters,'a'); // Suchalgorythmus
+			output("");
+			output("---- Binäre Suche: ");
+			output("searchIndex: " + searchIndex); // kleines a wird an Index 1 gefunden
 		
-		// 	// output("---- Suche: erfolglos ----");
-		// 	// ... danach Suche (2)
-		// 	searchIndex = Arrays.binarySearch(letters,'e');
-		// 	output("searchIndex: " + searchIndex);  // -(length+1)
-		// 	// .... isInArray()
+			// output("---- Suche: erfolglos ----");
+			// ... danach Suche (2)
+			searchIndex = Arrays.binarySearch(letters,'e');
+			output("searchIndex: " + searchIndex);  // -(length+1) wird ausgegeben wenn nix gefunden
+			// .... isInArray()
 			
-		// 	output("---- fill() ----");
-		// 	//.. Array füllen
-		// 	Arrays.fill(letters, '0');
-		// 	for (int i = 0; i < letters.length; i++) {
-		// 		output((letters[i]));
-		// 	}
+			output("---- fill() ----"); // füllen
+			//.. Array füllen
+			Arrays.fill(letters, '0');
+			for (int i = 0; i < letters.length; i++) {
+				output((letters[i]));
+			}
 
     }
 
@@ -96,7 +96,7 @@ public class App {
 
 	// überladene Methode
 	private static void output(char outChar) {
-		System.out.print(outChar);
+		System.out.print(outChar); //kein println, das heisst Elemente sind in einer Reihe
     }
 
 
